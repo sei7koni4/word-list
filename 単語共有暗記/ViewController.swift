e4f343b0df9ac215e791a6305ddc746a303110b6
小西佳子 <konishiyoshiko@konishiyoshiko-no-macbook-air.local>
2017-07-01 15:57:59 +0900

    Initial Commit

---
+++ 単語共有暗記/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  単語共有暗記
+//
+//  Created by 小西佳子 on 2017/07/01.
+//  Copyright © 2017年 小西星七. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
