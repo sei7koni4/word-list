94d82cc8f7597ec5ab729129344c833c40bafb46
小西佳子 <hoheto.star7@gmail.com>
2018-07-21 17:59:45 +0900

    first commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target '単語共有暗記' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for 単語共有暗記
+
+end
---
+++ 単語共有暗記/View17Controller.swift
@@ -0,0 +1,35 @@
+//
+//  View17Controller.swift
+//  単語共有暗記
+//
+//  Created by 小西佳子 on 2017/11/04.
+//  Copyright © 2017年 小西星七. All rights reserved.
+//
+
+import UIKit
+
+class View17Controller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
